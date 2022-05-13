aafa4e0d2e8aaa1f9173b041fe660cb62f6cd5e0
jurandarjanovic <darjan.juranovic@gmail.com>
2022-05-13 17:51:55 +0200

    Update BOLNICA.sql

--- BOLNICA.sql
+++ BOLNICA.sql
@@ -13,14 +13,14 @@ CREATE TABLE medicinske_sestre(
 	id INTEGER PRIMARY KEY,
     ime VARCHAR (20) NOT NULL,
     prezime VARCHAR (30) NOT NULL,
-    datum_rodenja DATE NOT NULL
+    datum_rodenja DATETIME NOT NULL
 );
 -- id 200 do 300
 CREATE TABLE pacijent (
 	id INTEGER PRIMARY KEY,
     ime VARCHAR(20) NOT NULL,
     prezime VARCHAR(30) NOT NULL,
-    datum_rodenja DATE NOT NULL,
+    datum_rodenja DATETIME NOT NULL,
     adresa VARCHAR (40) NOT NULL,
     spol CHAR (1) NOT NULL,
     broj_telefona VARCHAR (20) NOT NULL UNIQUE,
@@ -40,7 +40,7 @@ CREATE TABLE recept (
 	id INTEGER PRIMARY KEY,
     id_doktor INTEGER NOT NULL,
     id_pacijent INTEGER NOT NULL,
-    datum DATE NOT NULL,
+    datum DATETIME NOT NULL,
     FOREIGN KEY (id_doktor) REFERENCES doktor (id),
     FOREIGN KEY (id_pacijent) REFERENCES pacijent (id)
 );
@@ -78,7 +78,7 @@ CREATE TABLE dezurstvo (
 	id INTEGER PRIMARY KEY,
     id_medsestra INTEGER NOT NULL,
     id_odjel INTEGER NOT NULL,
-    datum_dezurstva DATE NOT NULL,
+    datum_dezurstva DATETIME NOT NULL,
     FOREIGN KEY (id_medsestra) REFERENCES medicinske_sestre (id),
     FOREIGN KEY (id_odjel) REFERENCES odjel (id)
 );
@@ -86,8 +86,8 @@ CREATE TABLE dezurstvo (
 CREATE TABLE zaposleni (
 	id INTEGER PRIMARY KEY,
     id_odjel INTEGER NOT NULL,
-    datum_zaposlenja DATE NOT NULL,
-    datum_kraja_rada DATE NOT NULL,
+    datum_zaposlenja DATETIME NOT NULL,
+    datum_kraja_rada DATETIME NOT NULL,
     FOREIGN KEY (id_odjel) REFERENCES odjel (id)
 );
 -- id 1100 do 1200
@@ -97,8 +97,8 @@ CREATE TABLE oprema (
     id_soba INTEGER NOT NULL,
     naziv VARCHAR(20) NOT NULL,
     sifra VARCHAR(20) NOT NULL,
-    datum_zaprimanja DATE NOT NULL,
-    datum_otpisa DATE NOT NULL,
+    datum_zaprimanja DATETIME NOT NULL,
+    datum_otpisa DATETIME NOT NULL,
     FOREIGN KEY (id_odjel) REFERENCES odjel (id),
     FOREIGN KEY (id_soba) REFERENCES soba (id)
 );
@@ -109,7 +109,7 @@ CREATE TABLE posjeta (
     prezime VARCHAR(30) NOT NULL,
     id_pacijent INTEGER NOT NULL,
     id_soba INTEGER NOT NULL,
-    datum DATE NOT NULL,
+    datum DATETIME NOT NULL,
     vrijeme_dolaska TIME NOT NULL,
     vrijeme_odlaska TIME NOT NULL,
     FOREIGN KEY (id_pacijent) REFERENCES pacijent (id),
